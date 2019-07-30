2434e63ac0bb708746a7ba1fd7c720c2a367df49
zwting <zuoweitaoting@gmail.com>
2019-07-31 00:32:59 +0800

    第一次提交

---
+++ LearnOpenGL/main.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* win);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* win = glfwCreateWindow(SCREEN_WIDTH,SCREEN_HEIGHT, "LearnOpenGL", nullptr, nullptr);
+	if (win == nullptr)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(win);
+	glfwSetFramebufferSizeCallback(win, framebuffer_size_callback);
+	if (!gladLoadGLLoader(reinterpret_cast<GLADloadproc>(glfwGetProcAddress)))
+	{
+		int code = glfwGetError(nullptr);
+		std::cout << "Failed to initialize GLAD" << code << std::endl;
+		char buffer[1024];
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(win))
+	{
+		processInput(win);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(win);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* win)
+{
+	if (glfwGetKey(win, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(win, true);
+	}
+}
