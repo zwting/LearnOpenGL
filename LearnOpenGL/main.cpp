81a7002392b570870d58a97a48d8bae23c585965
unknown <weitao.zuo@rastar.gd>
2019-07-31 14:35:16 +0800

    添加GLFW3头文件和库文件；添加glad

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -1,6 +1,7 @@
-﻿#include <iostream>
-#include <glad/glad.h>
+﻿#include "glad/glad.h"
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* win);
